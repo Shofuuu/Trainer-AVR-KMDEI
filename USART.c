fd27438a7b9d4836a1b76e0b1bb3bdbe1f809dcd
Muh. Shofuwan Anwar <44674870+Shofuuu@users.noreply.github.com>
2019-09-08 08:23:34 +0700

    Create USART.c

---
+++ USART.c
@@ -0,0 +1,39 @@
+#ifndef F_CPU
+	#define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void USART_init(int baudrate){
+	int ubrr = ((F_CPU)/(baudrate*16UL)-1);
+
+	UBRRL = (unsigned char)(ubrr);
+	UBRRH = (ubrr>>8);
+	UCSRB |= (1<<RXEN) | (1<<TXEN);
+	UCSRC |= (1<<URSEL) | (1<<UCSZ1) | (1<<UCSZ0);
+}
+
+unsigned char USART_read(void){
+	while(!(UCSRA & (1<<RXC)));
+	return UDR;
+}
+
+void USART_write(char c){
+	while(!(UCSRA & (1<<UDRE)));
+	UDR = c;
+}
+
+void USART_writes(char* s){
+	while(*s){
+		USART_write(*s);
+		s++;
+	}
+}
+
+int main(){
+	USART_init(9600);
+	USART_writes("KMDEI Keluarga Mahasiswa Diploma Elektronika dan Instrumentasi\r\n");
+
+	while(1);
+}
